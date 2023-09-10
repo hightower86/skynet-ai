90798982aa9f681fb6da7ef07aef3ca0988e21ae
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-09-10 16:02:48 +0400

    Sidebar menu

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/sidebar';
 import Navbar from '@/components/navbar';
 import { FC } from 'react';
 
@@ -9,7 +10,7 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div className="text-white text-4xl">Hello Sidebar</div>
+                <Sidebar />
             </div>
             <main className="md:pl-72">
                 <Navbar />
