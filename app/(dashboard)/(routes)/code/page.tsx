130b3d5d2db6b0941f62f055bcd4746ee2958866
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-09-13 18:37:24 +0400

    Code page and code route

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,165 @@
+'use client';
+import axios from 'axios';
+import { Heading } from '@/components/heading';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { formSchema } from './constants';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { ChatCompletionRequestMessage } from 'openai';
+import { cn } from '@/lib/utils';
+import { Loader } from '@/components/loader';
+import { Empty } from '@/components/ui/empty';
+import { UserAvatar } from '@/components/user-avatar';
+import { BotAvatar } from '@/components/bot-avatar';
+import { Code } from 'lucide-react';
+import ReactMarkdown from 'react-markdown';
+
+const CodePage = ({}) => {
+    const router = useRouter();
+
+    const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>(
+        []
+    );
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        defaultValues: {
+            prompt: '',
+        },
+        resolver: zodResolver(formSchema),
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const userMessage: ChatCompletionRequestMessage = {
+                role: 'user',
+                content: values.prompt,
+            };
+            const newMessages = [...messages, userMessage];
+
+            const response = await axios.post('/api/code', {
+                messages: newMessages,
+            });
+            setMessages((current) => [...current, userMessage, response.data]);
+
+            form.reset();
+        } catch (error) {
+            // TODO: Open Pro Modal
+        } finally {
+            router.refresh();
+        }
+        console.log({ values });
+    };
+
+    return (
+        <div>
+            <Heading
+                title="Code"
+                description="Generate code using descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="
+                            w-full
+                            p-4
+                            px-3
+                            md:px-6
+                            focus-within:shadow-sm
+                            grid
+                            grid-cols-12
+                            gap-2
+                            rounded-lg
+                            border
+                            "
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="Simple toggle modal using react hooks."
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button
+                                type="submit"
+                                // size="icon"
+                                className="col-span-12 lg:col-span-2"
+                                disabled={isLoading}
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                            <Loader />
+                        </div>
+                    )}
+                    {messages.length === 0 && !isLoading && (
+                        <Empty label="No Code started." />
+                    )}
+                    <div className="flex flex-col-reverse gap-y-4">
+                        {messages.map((message) => (
+                            <div
+                                key={message.content}
+                                className={cn(
+                                    'p-8 w-full flex items-start gap-x-8 rounded-lg',
+                                    message.role === 'user'
+                                        ? 'bg-white border border-black/10'
+                                        : 'bg-muted'
+                                )}
+                            >
+                                {message.role === 'user' ? (
+                                    <UserAvatar />
+                                ) : (
+                                    <BotAvatar />
+                                )}
+                                <ReactMarkdown
+                                    components={{
+                                        pre: ({ node, ...props }) => (
+                                            <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                                <pre {...props} />
+                                            </div>
+                                        ),
+                                        code: ({ node, ...props }) => (
+                                            <code
+                                                className="bg-black/10 rounded-lg p-1"
+                                                {...props}
+                                            />
+                                        ),
+                                    }}
+                                    className="text-sm overflow-hidden leading-7"
+                                >
+                                    {message.content || ''}
+                                </ReactMarkdown>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CodePage;
